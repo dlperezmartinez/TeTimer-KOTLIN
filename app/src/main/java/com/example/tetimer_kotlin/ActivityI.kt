81100597e91a091dd666fd9618affafb7f880885
dlperezmartinez <dlperezmartinez@gmail.com>
2021-11-04 12:13:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tetimer_kotlin/ActivityI.kt
@@ -0,0 +1,11 @@
+package com.example.tetimer_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
